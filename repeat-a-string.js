db07610e9e3ce20cc2d335c72467dfbdb19cc44c
techEmprez <choiceosobor@gmail.com>
2022-09-24 07:06:12 +0100

    write a function

--- repeat-a-string.js
+++ repeat-a-string.js
@@ -1,8 +1,1 @@
 function repeatStringNumTimes(str, num) {
-  if (num < 1) return '';
-  return num === 1 ? str : str + repeatStringNumTimes(str, num - 1);
-}
-
-repeatStringNumTimes('abc', 3);
-
-module.exports = repeatStringNumTimes;
